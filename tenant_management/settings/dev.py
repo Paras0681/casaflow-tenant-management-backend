4885a5192bfd49f958b7afcce6b816ece55698c9
Paras0681 <paraskamble8268@gmail.com>
2025-09-25 00:00:23 +0530

    feat: added hosts

--- tenant_management/settings/dev.py
+++ tenant_management/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *
 from decouple import config
 
 DEBUG = True
-ALLOWED_HOSTS = [f"{config("WEBSERVICE_NAME")}"]
+ALLOWED_HOSTS = [f"{config("WEB_SERVICE_NAME")}"]
 
 DATABASES = {
     'default': {
