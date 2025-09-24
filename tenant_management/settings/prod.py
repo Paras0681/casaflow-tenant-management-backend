9cdbe44ae0cddf7f30826042c179363c3a8180ad
Paras0681 <paraskamble8268@gmail.com>
2025-09-24 20:57:34 +0530

    fix: deployment issues

--- tenant_management/settings/prod.py
+++ tenant_management/settings/prod.py
@@ -7,8 +7,8 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
+        'NAME': 'koyebdb',
+        'USER': 'koyeb-adm',
         'PASSWORD': config("DB_PASSWORD"),
         'HOST': config("DB_HOST"),
         'OPTIONS': {'sslmode': 'require'},
