37ac2385135e7a6ed317465d7096bf2a135f9869
Paras0681 <paraskamble8268@gmail.com>
2025-09-27 19:10:42 +0530

    fix: asgi and wsgi .env updates.

--- tenant_management/asgi.py
+++ tenant_management/asgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from decouple import config
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tenant_management.settings.dev")
-
+django_env = config("DJANGO_ENV", default="local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"tenant_management.settings.{django_env}")
 application = get_asgi_application()
--- tenant_management/wsgi.py
+++ tenant_management/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from decouple import config
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tenant_management.settings.dev")
+django_env = config("DJANGO_ENV", default="local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"tenant_management.settings.{django_env}")
 
 application = get_wsgi_application()
